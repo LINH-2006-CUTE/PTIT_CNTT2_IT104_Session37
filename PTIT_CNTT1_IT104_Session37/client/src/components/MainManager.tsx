d355c9cba6d3d2e27ed21da73a086a972d241ef5
Lê Nhật Linh <lenhatlinh19042006@nh19042006gmail.42006gmailcom>
2025-09-28 19:48:33 +0700

    2

---
+++ PTIT_CNTT1_IT104_Session37/client/src/components/MainManager.tsx
@@ -0,0 +1,13 @@
+import HeaderForm from "./HeaderForm";
+import FilterForm from "./FilterForm";
+import StudentList from "./StudentList";
+
+export default function MainManager() {
+  return (
+    <div className="flex flex-col gap-3 justify-center w-[80%] mx-[auto] pt-4 bg-[#F9FAFB]">
+      <HeaderForm />
+      <FilterForm />
+      <StudentList/>
+    </div>
+  );
+}
